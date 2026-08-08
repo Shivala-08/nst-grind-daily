86788674fd1653967ba73bea8751101929930881
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-08-09 00:58:03 +0530

    Sync Solution: Dev Club - Create Member Display Line - JS Practice Questions [Score: 100%]

---
+++ newton-school-course/dev-club-create-member-display-line-js-practice-questions/z48u7in3yq7r.js
@@ -0,0 +1,5 @@
+// The platform provides 'name', 'domain', and 'level' automatically.
+// Just create the formatted string and print it.
+
+let finalOutput = `Dev Club | Name: ${name} | Domain: ${domain} | Level: ${level}`;
+console.log(finalOutput);
