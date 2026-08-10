c356e0eca5b3d725bfe6601ea1600bf2ae2a89ce
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-08-10 15:43:45 +0530

    Sync Solution: The String Tool - JS Practice Questions [Score: 100%]

---
+++ newton-school-course/the-string-tool-js-practice-questions/zajwkxwqdghq.js
@@ -0,0 +1,3 @@
+// Your code logic goes here, function name should be getInitials with 2 parameter, you don't need to print anything just return the initial of firstName and lastName. 
+// Do not call the function
+const getInitials = (firstName, lastName) => (firstName[0] + lastName[0]).toUpperCase();
