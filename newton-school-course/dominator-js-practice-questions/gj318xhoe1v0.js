9f7724474dd58ac582fb7d2e844f662c889d96d0
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-08-14 12:51:11 +0530

    Sync Solution: Dominator - JS Practice Questions [Score: 100%]
    Implement the given JavaScript function named extract, that takes a 2D array (array of arrays) as input and returns a new 2D array where:
    
     Each inner array contains only the even numbers.
    
     After filtering out odd numbers, only non-empty inner arrays with an even length are included in the final result.
    
    Constraints:
    
     You must not use loops (for, while, do while, for in, for of).
    
     Instead, use array methods such as .map(), .filter(), .reduce(), or .forEach().
    
    ### Input
    A 2D array of numbers
    
    ### Output
    A filtered array which satisfies the given conditions
    
    ### Example
    Input:
    [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
    
    Output:
    [ [ 2, 4 ], [ 6, 8 ], [ 10, 12 ] ]
    
    Explanation:
    Filter even numbers from each inner array:
    [1,2,3,4] → [2,4]
    [5,6,7,8] → [6,8]
    [9,10,11,12] → [10,12]
    
    Check the length of filtered arrays:
    [2,4] → Length = 2 (Even) → ✅ Included
    [6,8] → Length = 2 (Even) → ✅ Included
    [10,12] → Length = 2 (Even) → ✅ Included
    
    Input:
    [[2,4,6],[8,10,12,14]]
    
    Output:
    [ [ 8, 10, 12, 14 ] ]
    
    Explanation:
    Filter even numbers from each inner array:
    [2,4,6] → [2,4,6]
    [8,10,12,14] → [8,10,12,14]
    
    Check the length of filtered arrays:
    [2,4,6] → Length = 3 (Odd) → ❌ Removed
    [8,10,12,14] → Length = 4 (Even) → ✅ Included

--- newton-school-course/dominator-js-practice-questions/gj318xhoe1v0.js
+++ newton-school-course/dominator-js-practice-questions/gj318xhoe1v0.js
@@ -1,5 +1,5 @@
 function extract(arr) {
     return arr
         .map(innerArray=>innerArray.filter(num =>num%2==0))
-        
+        filter(innerArray => innerArray.length > 0 && innerArray.length % 2 === 0)
 }
