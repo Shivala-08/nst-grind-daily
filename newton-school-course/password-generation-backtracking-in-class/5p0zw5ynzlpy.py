5b97ed951a3c881d9beb334a0ddb9162bbc33287
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-08-31 12:19:13 +0530

    Sync Solution: Password Generation - Backtracking - In Class [Score: 100%]
    Given an integer n, generate all possible passwords of length n using the character set:
    
    chars = "aeiou1234@"
    
    Characters can be reused any number of times.
    
    Print all possible passwords.
    
    ### Input
    A single integer n, representing the length of each password.
    
    ### Output
    Print every possible password of length n. (you can print in any order)
    
    Print each password on a new line.
    
    ### Example
    Example 1:
    
    Input
    
    1
    
    Output
    
    a
    e
    i
    o
    u
    1
    2
    3
    4
    @
    
    Explanation
    For n = 1, each character from the given character set forms a password.
    
    The passwords are printed in the same order as the characters appear in:
    
    aeiou1234@

---
+++ newton-school-course/password-generation-backtracking-in-class/5p0zw5ynzlpy.py
@@ -0,0 +1,17 @@
+import itertools
+import sys
+
+def solve():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    
+    n = int(input_data[0])
+    chars = "aeiou1234@"
+    
+    # Generate all Cartesian products of length n with repetition
+    for p in itertools.product(chars, repeat=n):
+        print("".join(p))
+
+if __name__ == '__main__':
+    solve()
