a2414c255c1ce645d313218c832b25bfca9c2fd5
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 14:57:43 +0530

    Sync Solution: Average marks of the class - JS Practice Questions [Score: 100%]
    You are given an array of student objects, where each object represents a student and contains:
    
     * name: the student&rsquo;s name
    
     * marks: an array of 5 subject marks, each out of 10
    
    Complete the JavaScript Function filterStudents to determine whether each student passes or fails based on the following rule:
    
     * If the average of the 5 subjects marks is greater than or equal to 35, the student Passes
    
     * Otherwise, the student Fails
    
    Add the students who passed the exam in the given users array and return it.
    
    ### Input
    An array of student details. Each student will be in the form of an object (Example of a user detail { name: "Rahul", marks: [40, 38, 42, 30, 35] } )
    
    ### Output
    Print an array of student names who passed the exam.
    
    ### Example
    Sample Input1:
    [
     { name: "Rahul", marks: [40, 38, 42, 30, 35] },
     { name: "Aisha", marks: [25, 30, 20, 28, 22] },
     { name: "Ken", marks: [45, 50, 40, 38, 42] }
    ]
    
    Sample Output1
    ['Rahul','Ken']

---
+++ newton-school-course/average-marks-of-the-class-js-practice-questions/n4t2n02awdpv.js
@@ -0,0 +1,11 @@
+function calculateGrade(score) {
+    if (score >= 90) {
+        console.log('A');
+    } else if (score >= 75) {
+        console.log('B');
+    } else if (score >= 60) {
+        console.log('C');
+    } else {
+        console.log('D');
+    }
+}
