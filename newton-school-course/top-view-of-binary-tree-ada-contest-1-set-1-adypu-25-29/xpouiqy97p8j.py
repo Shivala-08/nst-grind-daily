53eb5bce85c7367a520f742fc59c2a96f9bb0d4f
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 16:38:58 +0530

    Sync Solution: Top View of Binary Tree - ADA | Contest 1 | Set 1 | ADYPU | 25-29 [Score: 100%]
    Given a Binary Tree, print the Top view of it. Top view of a Binary Tree is a set of nodes visible when the tree is viewed from the top. The Top view of the following tree is 4 2 1 3 7. 
    
    ### Input
    Your Task:
    This is a function problem. You don't have to take input. Just complete the function printTopView() that takes a node as a parameter and prints the right view. Also, the functional part visible to you contains information about the Node class
    
    Constraints:
    1 &le; T &le; 100
    1 &le; N &le; 105
    1 &le; node values &le; 105
    
    Sum of "N" over all test cases does not exceed 105
    
    For Custom Input:
    The first line of input should contain the number of test cases T. For each test case, there will be two lines of input.
    The first line contains a number of nodes N. Second line will be a string representing the tree as described below: 
    The values in the string are in the order of level order traversal of the tree where, numbers denote node values, and a character “N” denotes NULL child.
    Note: If a node has been declared Null using 'N', no information about its children will be given further in the array.
    
    ### Output
    Print the top view of the binary tree.
    
    ### Example
    Sample Input
    2
    3
    1 2 3
    5
    10 20 30 40 60
    Sample Output
    2 1 3
    40 20 10 30 
    Explanation
    Test case 1: Below is the given tree
    
    For the above test case, the top view is: 3 1 2
    Test case 2: Below is the given tree
    
    Top View is: 40 20 10 30.

--- newton-school-course/top-view-of-binary-tree-ada-contest-1-set-1-adypu-25-29/xpouiqy97p8j.py
+++ newton-school-course/top-view-of-binary-tree-ada-contest-1-set-1-adypu-25-29/xpouiqy97p8j.py
@@ -1,49 +1,32 @@
 from collections import deque
 
-'''
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-'''
-
 def printTopView(root):
     if not root:
         return
     
-    # Dictionary to store the top-most node at each horizontal distance
     top_view_map = {}
-    
-    # Queue stores tuples of (node, horizontal_distance)
     queue = deque([(root, 0)])
-    
     min_hd = 0
     max_hd = 0
     
     while queue:
         node, hd = queue.popleft()
         
-        # If this is the first time we are seeing this horizontal distance, record the node
         if hd not in top_view_map:
             top_view_map[hd] = node.data
         
-        # Track minimum and maximum horizontal distances for printing later
         min_hd = min(min_hd, hd)
         max_hd = max(max_hd, hd)
         
-        # Push left child with HD - 1
         if node.left:
             queue.append((node.left, hd - 1))
             
-        # Push right child with HD + 1
         if node.right:
             queue.append((node.right, hd + 1))
             
-    # Print the top view nodes from the leftmost horizontal distance to the rightmost
     result = []
     for hd in range(min_hd, max_hd + 1):
         if hd in top_view_map:
             result.append(str(top_view_map[hd]))
             
-    print(" ".join(result))
+    print(" ".join(result), end="")
