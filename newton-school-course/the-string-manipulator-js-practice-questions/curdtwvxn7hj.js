f78e944e519b15a6049a8680191980c6f73d7adf
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 14:48:00 +0530

    Sync Solution: The String Manipulator - JS Practice Questions [Score: 100%]
    You have joined a small team of developers working on a futuristic text-processing AI called LexiBot.
    
    LexiBot doesn&rsquo;t know how to transform text by itself &mdash; instead, it depends entirely on callback functions provided by developers like you.
    
    Your team has already built a Higher-Order Function (HOF) named stringProcessor.
    
    This function takes two things:
    
     * 
     A string
    
     
    
     * 
     A callback function that decides how that string should be modified
    
     
    
    Here&rsquo;s what the team has written so far:
    
    function stringProcessor(str, callback) {
     return callback(str);
    }
    
    LexiBot has now assigned you a task:
    
    Your Mission
    
    You must create two different callback functions that LexiBot will use to process its incoming text messages.
    
     * 
     Callback 1: toUpper
    
     
     * 
     Your job is to write a callback function that receives a string
    
     
    
     * 
     And returns that string in ALL UPPERCASE LETTERS
    
     
    
     
     
    
     * 
     Callback 2: exclaim
    
     
     * 
     This callback should receive a string
    
     
    
     * 
     And return the same string but with an exclamation mark (!) at the end
    
     
    
     
     
    
    LexiBot will then test your callbacks by calling:
    
    stringProcessor("hello", toUpper); // Expected: HELLO
    stringProcessor("hello", exclaim); // Expected: hello!
    
    Your task is to write the two callbacks exactly as expected, so LexiBot can successfully process text using its HOF.
    
    ### Input
    Input will consist of 2 values seprated by space
    1st value will be the string that need to be processed. 
    2nd value will be the name of callback function (i.e. toUpper or exclaim) need to be pass to the stringProcessor function 
    
    example 1: Hello toUpper
    example 2: Hello exclaim
    
    ### Output
    Based on the callback function passed to stringProcessor function will get following output.
    
    When toUpper is passed, the output must be the UPPERCASE version of the string.
    Example: HELLO
    
    When exclaim is passed, the output must be the original string + "!".
    Example: hello!
    
    ### Example
    Input: hello toUpper
    Output: HELLO
    
    Input: hello exclaim
    Output: hello!

---
+++ newton-school-course/the-string-manipulator-js-practice-questions/curdtwvxn7hj.js
@@ -0,0 +1,14 @@
+// HOF - DO NOT MODIFY
+function stringProcessor(str, callback) {
+    return callback(str);
+}
+
+// TODO: Write a callback 'toUpper' that returns a string in all uppercase.
+function toUpper(str) {
+    return str.toUpperCase();
+}
+
+// TODO: Write a callback 'exclaim' that adds an exclamation mark to the end.
+function exclaim(str) {
+    return str + "!";
+}
