6256dd37e256cdcd4564bb75b72043a0c67ce528
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 13:07:56 +0530

    Sync Solution: Print all root to leaf paths - Backtracking - Post Class [Score: 100%]
    Given the root of a binary tree, return all root to leaf paths.
    
     
    
    ### Input
    Since this is a functional problem, you don't have to input anything. You just need to complete the rootToLeafPaths().
    
    You are given root node of the binary tree as function parameter.
    
    ### Output
    Return all the paths from root to leaf .
    
    Custom Input Output
    First line contains an integer value 'N' indicating number of nodes(including null) in binary tree.
    Second line will be a string representing the tree as described below: 
    The values in the string are in the order of pre-order traversal of the tree where, numbers denote node values, and -1 denotes NULL child.
    
    Note: If a node has been declared Null using -1, no information about its children will be given further in the array.
    
    ### Example
    Input:
    11
    50 25 -1 -1 12 37 -1 -1 30 -1 -1
    
    Output:
    50 25
    50 12 37
    50 12 30

---
+++ newton-school-course/print-all-root-to-leaf-paths-backtracking-post-class/qx4y2466uleb.py
@@ -0,0 +1,34 @@
+'''
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+'''
+
+def rootToLeafPaths(root):
+    result = []
+    
+    def dfs(node, current_path):
+        if not node:
+            return
+        
+        # Add the current node's data to the path
+        current_path.append(str(node.data))
+        
+        # If it's a leaf node, join the path and add to results
+        if not node.left and not node.right:
+            result.append(" ".join(current_path))
+        else:
+            # Recursively traverse left and right subtrees
+            dfs(node.left, current_path)
+            dfs(node.right, current_path)
+            
+        # Backtrack to explore other paths
+        current_path.pop()
+
+    if root:
+        dfs(root, [])
+        
+    for path in result:
+        print(path)
