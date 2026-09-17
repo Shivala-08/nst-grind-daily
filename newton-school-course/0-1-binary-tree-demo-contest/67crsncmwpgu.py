f4addc4f6099db5d7874310ef90d4c6541353793
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 16:30:40 +0530

    Sync Solution: 0-1 Binary tree - Demo Contest [Score: 100%]
    Given the root of binary tree of binary values i.e. value of all nodes are either 0 or 1. Count the number of subtrees where number of nodes with value 1 is strictly greater than the number of nodes with value 0.
    
    ### Input
    You don't have to input anything, just complete the given function.
    
     Constrains 
    1 5
    0 <= node's value <= 1
    
    ### Output
    Return the number of required subtrees.
    
    ### Example
    Input:
    1 1 0 null 0 1
    
    Output:
    2
    
    Explanation : 
     1 (1)
     / \
     (2) 1 0 (3)
     \ \
     (3) 0 1 (5)
    
    Subtrees rooted at node 5 and 1 have more number of 1 nodes than the number of 0 nodes.

---
+++ newton-school-course/0-1-binary-tree-demo-contest/67crsncmwpgu.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class Node:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def countSubtrees(self, root):
+        self.valid_subtrees = 0
+        
+        def dfs(node):
+            if not node:
+                return 0, 0  # (count_ones, count_zeros)
+            
+            # Recurse left and right subtrees
+            left_ones, left_zeros = dfs(node.left)
+            right_ones, right_zeros = dfs(node.right)
+            
+            # Current node values
+            current_ones = 1 if node.val == 1 else 0
+            current_zeros = 1 if node.val == 0 else 0
+            
+            # Total ones and zeros in the current subtree
+            total_ones = left_ones + right_ones + current_ones
+            total_zeros = left_zeros + right_zeros + current_zeros
+            
+            # Check the condition
+            if total_ones > total_zeros:
+                self.valid_subtrees += 1
+                
+            return total_ones, total_zeros
+        
+        dfs(root)
+        return self.valid_subtrees
