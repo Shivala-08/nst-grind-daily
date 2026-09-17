d860840be3d295b43f19d91690f7ca0aee35ff64
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 13:11:50 +0530

    Sync Solution: Find Subsets - Backtracking, Backtracking - In Class [Score: 100%]
    Given an integer array nums of unique elements, return all possible subsets (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.
    
    ### Input
    User Task
    This is a function problem. You don't have to take any input. You are required to complete the function findSubsets() which takes an array nums as parameter.
    
    Custom Input
    The first line will take a single integer n.
    The second line will take n space-separated integers representing elements of the nums array.
    
    Constraints:
     1 \le n \le 10 
     -10 \le nums[i] \le 10 
    
    ### Output
    Return all possible subsets (the power set). (Return list of sublists)
    
    ### Example
    Input
    3
    -10 5 -6
    Output
    []
    [-10]
    [-10 -6]
    [-10 5]
    [-10 5 -6]
    [-6]
    [5]
    [5 -6]
    
    Input
    1
    0
    Output
    []
    [0]

---
+++ newton-school-course/find-subsets-backtracking-backtracking-in-class/zcfwe0k58niy.py
@@ -0,0 +1,12 @@
+def findSubsets(nums):
+    result = []
+    
+    def backtrack(start, current):
+        result.append(list(current))
+        for i in range(start, len(nums)):
+            current.append(nums[i])
+            backtrack(i + 1, current)
+            current.pop()
+            
+    backtrack(0, [])
+    return result
