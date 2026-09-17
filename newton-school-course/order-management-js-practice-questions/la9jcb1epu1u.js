84fb7c80d1e8cb01189aa4ec0f2af3212e3379ee
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 19:01:15 +0530

    Sync Solution: Order Management - JS Practice Questions [Score: 100%]
    You are a waiter at a fine restaurant where two types of people come old people and young people. Both of them give you their order with the time period required to make their dishes. Now the difference between both of them is if an old person gives you an order then you can&#39;t take other people&#39;s order and you have to wait till the order of old person gets fulfilled in the required time, but with the case of young people you can take their orders and then while their order is preparing you can take other people&#39;s order and whichever order has less time to get prepared comes first.
    
    Your task is to complete three functions,
    
     * first function OldPerson() that take input as name of old person and time for his order to get prepared and returns the name of the person after the designated time.
    
     * Second YoungPerson() that takes input name of young person and time for his order to get prepared and returns the name of the person after the designated time.
    
     * Last the execution() in which you call the functions of OldPerson(), YoungPerson() using async await and call them in such way so that it first execute the order of OldPerson and wait till their order gets executed and then take order of two Young Person and return whichever takes less time.
    
     
    
    ### Input
    function OldPerson() that take input as name of old person which is String and time which is in millseconds and in Integer Format
    function YoungPerson() that take input as name of young person which is String and time which is in millseconds and in Integer Format
    
    ### Output
    function OldPerson() returns name of old person after waiting time specified in function is over
    function YoungPerson() returns name of young person after waiting time specified in function is over
    
    ### Example
    const name1="Rahul";
    const time1=1000;
    const answer1= YoungPerson(name1,time1) // returns name after waiting for time time1 millisecond
    console.log(answer1); // prints Rahul after 1second
    
    const name2="Mr.Kumar";
    const time2=2000;
    const answer2= OldPerson(name2,time2) // returns name after waiting for time time2 millisecond
    console.log(answer2); // prints Mr.Kumar after 2second
    
    const oldPersonName="Mr.Vargis",
    oldPersonTime=2000,
    person1Name="Jatin",
    person1Time=3000,
    person2Name="Piyush",
    person2Time=1000;
    execution(oldPersonName,oldPersonTime,person1Name,person1Time,person2Name,person2Time)
    // waits for 2000ms then prints Mr.Vargis
    //prints Piyush after 1000ms 
    //prints Jatin after 3000ms

--- newton-school-course/order-management-js-practice-questions/la9jcb1epu1u.js
+++ newton-school-course/order-management-js-practice-questions/la9jcb1epu1u.js
@@ -15,12 +15,9 @@ const YoungPerson = (name, time) => {
 };
 
 async function execution(oldPersonName, oldPersonTime, person1Name, person1Time, person2Name, person2Time) {
-  // 1. Execute the old person's order first and wait for it to complete
   const oldResult = await OldPerson(oldPersonName, oldPersonTime);
   console.log(oldResult);
 
-  // 2. Take the orders of the two young persons concurrently using Promise.race() 
-  // to find out which one finishes first (takes less time)
   const fastestYoung = await Promise.race([
     YoungPerson(person1Name, person1Time),
     YoungPerson(person2Name, person2Time)
