887c8e46709f0d6319f5c91f9a55b03800f1e440
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 18:35:20 +0530

    Sync Solution: Chain Some Math with Promises - JS Practice Questions [Score: 100%]
    You are given a number value.
    
    Your task is to perform a series of operations on the number using Promises and .then() chaining.
    
    The operations should be executed in the following order:
    
     Double the number
    
     Add 10 to the result
    
     Multiply the result by 3
    
    Each operation must be written in a separate function that returns a Promise.
    
    Functions to Create:
    
     double(value): Returns a Promise that resolves with the doubled value.
    
     addTen(value): Returns a Promise that resolves with the value increased by 10.
    
     multiplyByThree(value): Returns a Promise that resolves with the value multiplied by 3.
    
    Requirements:
    
     Chain the functions using .then()
    
     Return the result from each .then() so it can be passed to the next function
    
     In the final .then(), print the result using console.log()
    
    ### Input
    Input: You will be given a single number, value.
    
    ### Output
    After performing all operations, log the final result.
    
    ### Example
    Input:
    5
    
    Output:
    60

---
+++ newton-school-course/chain-some-math-with-promises-js-practice-questions/y3ib1wpwj5go.js
@@ -0,0 +1,19 @@
+function double(value) {
+  return Promise.resolve(value * 2);
+}
+
+function addTen(value) {
+  return Promise.resolve(value + 10);
+}
+
+function multiplyByThree(value) {
+  return Promise.resolve(value * 3);
+}
+
+// Example usage to chain the promises:
+// double(5)
+//   .then(result => addTen(result))
+//   .then(result => multiplyByThree(result))
+//   .then(finalResult => {
+//     console.log(finalResult);
+//   });
