8db13b00d4721173da61948b5097e3e6aed2044d
Shivala-08 <89503779+Shivala-08@users.noreply.github.com>
2026-09-17 15:00:11 +0530

    Sync Solution: Sum of Odd - JS Practice Questions [Score: 100%]
    Given an Array of N integers, your task is to complete the function to return the sum of all odd numbers in the Array.
    
    function calculateSum(nums);
    Note: You are only required to complete the function and return the sum. You don&#39;t need to call the function or print the value.
    
    Constraints:
    
    1 <= N <= 1000
    
    1 <= nums[i] <= 1000 for 0<= i < N
    
    ### Input
    You are only required to complete the function and return the sum. You don't need to call the function or print the value.
    
    Input for testing:
    The first input line contains an Array(nums) containing N integers.
    
    ### Output
    The sum of all odd numbers in the Array nums
    
    ### Example
    Sample Input:-
    [1, 2, 3, 4]
    
    Sample Output:-
    4
    
    Sample Input:-
    [5, 5, 5]
    
    Sample Output:-
    15

---
+++ newton-school-course/sum-of-odd-js-practice-questions/rbre4kv9snqq.js
@@ -0,0 +1,9 @@
+function calculateSum(nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 !== 0) {
+            sum += nums[i];
+        }
+    }
+    return sum;
+}
